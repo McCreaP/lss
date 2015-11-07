d9db8a6e87c54286228ab6b579196efa68e8615d
Michal Zak <michal.zakm@gmail.com>
2015-11-07 14:39:14 +0100

    File logger handler

--- testing/system_tests/internals/logger_config.py
+++ testing/system_tests/internals/logger_config.py
@@ -1,20 +1,33 @@
+import os
 from colorlog import ColoredFormatter
 import logging
 
 LOGGER = logging.getLogger("test_runner")
 
-LOG_FORMAT = "%(log_color)s%(levelname)-8s%(reset)s | %(asctime)s | %(log_color)s%(message)s%(reset)s"
+COLOR_LOG_FORMAT = "%(log_color)s%(levelname)-8s%(reset)s | %(asctime)s | %(log_color)s%(message)s%(reset)s"
+MONOCHROMATIC_LOG_FORMAT = "%(levelname)-8s | %(asctime)s | %(message)s"
+
 
 def setup(verbose=False):
-    if verbose:
-        LOGGER.setLevel(logging.DEBUG)
-    else:
-        LOGGER.setLevel(logging.INFO)
-
-    formatter = ColoredFormatter(LOG_FORMAT)
-
+    LOGGER.setLevel(logging.DEBUG)
     handler = logging.StreamHandler()
+    if verbose:
+        handler.setLevel(logging.DEBUG)
+    else:
+        handler.setLevel(logging.INFO)
+
+    formatter = ColoredFormatter(COLOR_LOG_FORMAT)
     handler.setFormatter(formatter)
 
     LOGGER.addHandler(handler)
-    
+
+
+def add_file_handler(log_dir):
+    log_file = os.path.join(log_dir, 'test_runner.log')
+    handler = logging.FileHandler(log_file)
+    handler.setLevel(logging.DEBUG)
+
+    formatter = logging.Formatter(MONOCHROMATIC_LOG_FORMAT)
+    handler.setFormatter(formatter)
+
+    LOGGER.addHandler(handler)
--- testing/system_tests/test_runner.py
+++ testing/system_tests/test_runner.py
@@ -1,27 +1,39 @@
 #!/usr/bin/env python3
 import os
+import time
 import logging
 from argparse import ArgumentParser
 
+from internals import utils
 from internals import logger_config
 from internals.test import Test
 
-TESTS_DATA_PATH = './scenarios'
+TESTS_DATA_PATH = './scenarios/'
+
+LOG_ROOT_DIR = './logs/'
 
 LOGGER = logging.getLogger("test_runner")
 
 
 def parse_args():
-    parser = ArgumentParser("System tests runner")
-    parser.add_argument('test_name', metavar='TEST_NAME', nargs='?')
-    parser.add_argument(
-        '-l', '--list', action='store_true', help="List all tests names")
-    parser.add_argument(
-        '-v', '--verbose', action='store_true', help="Show more logs from running tests")
-    parser.add_argument(
-        '-p', '--executable-path', help="Specify scheduler executable path")
-    parser.add_argument(
-        '-i', '--input-dir', default='./run', help="Specify scheduler input directory")
+    parser = ArgumentParser(description="System tests runner")
+    parser.add_argument('scheduler',
+                        metavar='SCHEDULER',
+                        nargs=1,
+                        help="The scheduler executable path")
+    parser.add_argument('test_name',
+                        metavar='TEST_NAME',
+                        nargs='?',
+                        help="The name of a test to run. If it was not specified, all test will be run")
+    parser.add_argument('-l', '--list',
+                        action='store_true',
+                        help="List all tests names")
+    parser.add_argument('-v', '--verbose',
+                        action='store_true',
+                        help="Show more logs from running tests")
+    parser.add_argument('-i', '--input-dir',
+                        default='.',
+                        help="Specify scheduler input directory (default .)")
     return parser.parse_args()
 
 
@@ -29,19 +41,29 @@ def get_all_tests_names():
     return [f for f in os.listdir(TESTS_DATA_PATH)]
 
 
-def run_single_test(test_name, lss_input_dir):
+def make_log_dir(test_name):
+    log_dir = os.path.join(
+        LOG_ROOT_DIR, test_name + '_' + utils.human_readable_time(time.time()))
+    os.makedirs(log_dir, mode=0o775)
+    return log_dir
+
+
+def run_single_test(test_name, lss_input_dir, scheduler_path):
+    log_dir = make_log_dir(test_name)
+    logger_config.add_file_handler(log_dir)
     LOGGER.info("Test: %s ... ", test_name)
-    test = Test(TESTS_DATA_PATH, test_name, args.lss_input_dir, args.executable_path)
+    test = Test(
+        TESTS_DATA_PATH, test_name, lss_input_dir, scheduler_path, log_dir)
     test.run()
-    if test.has_failed:
+    if test.has_failed():
         LOGGER.error("Test: %s ... FAILED", test_name)
     else:
         LOGGER.info("Test: %s ... PASSED", test_name)
 
 
-def run_all_tests(lss_input_dir):
+def run_all_tests(lss_input_dir, scheduler_path):
     for test_name in get_all_tests_names():
-        run_single_test(test_name, lss_input_dir)
+        run_single_test(test_name, lss_input_dir, scheduler_path)
         LOGGER.info(60 * '=')
 
 
@@ -54,7 +76,8 @@ if __name__ == "__main__":
             print(test_name)
         exit(0)
 
+    scheduler_path = os.path.join(os.curdir, args.scheduler)
     if args.test_name:
-        run_single_test(args.test_name, args.lss_input_dir)
+        run_single_test(args.test_name, args.input_dir, scheduler_path)
     else:
-        run_all_tests(args.lss_input_dir)
+        run_all_tests(args.input_dir, scheduler_path)
