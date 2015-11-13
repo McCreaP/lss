c438f7e3fd07dd8a0166cadaa2cb3ade3a8c7d05
Kamil Zyla <7kamilzyla@gmail.com>
2015-11-13 07:07:41 +0100

    Prepare build setup for BasicReader unit tests.

---
+++ test/io/basic_input_test.cc
@@ -0,0 +1,13 @@
+#include "io/basic_input.h"
+
+#include <gtest/gtest.h>
+
+namespace lss {
+namespace io {
+
+TEST(BasicReaderTest, SetInputPath) {
+  EXPECT_TRUE(true);
+}
+
+}  // namespace io
+}  // namespace lss
