e4d6d8aca0ed66f559758f4d50233f12f95b755f
Michal Zak <michal.zakm@gmail.com>
2015-11-23 22:17:46 +0100

    Initial version of the pre push script
      - Try to build the project inside the Docker container
      - Verify the code by cpplint

---
+++ development_env/git_hooks/pre-push.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo -e "\e[32mRunning pre push script ... \e[0"
+echo `pwd`
+git stash -q --keep-index
+
+vagrant ssh --command "./lss/build.sh"
+[ $? -ne 0 ] && ABORT="TRUE"
+vagrant ssh --command "cpplint --filter=-legal/copyright \`find lss/src -type f\`"
+[ $? -ne 0 ] && ABORT="TRUE"
+
+git stash pop -q
+
+if [ $ABORT -eq "TRUE" ]
+then
+    echo "\e[31mRunning pre push script ... FAILED\e[0"
+    exit 1
+else
+    echo "\e[32mRunning pre push script ... DONE\e[0"
+    exit 0
+fi
