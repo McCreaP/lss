dd3ae5ebd5103af6375f250e8701c359b18e1f0a
Leonid Logvinov <logvinov.leon@gmail.com>
2016-02-09 15:04:04 +0000

    Move input operators to separate file

--- src/io/basic_input.cc
+++ src/io/basic_input.cc
@@ -107,50 +107,5 @@ bool BasicReader::Read(RawData* destination) {
   return true;
 }
 
-std::istream& operator>>(std::istream& input, Job& job) {
-  input >> job.id >> job.batch_id >> job.duration >> job.machineset_id;
-  for (int i = 0; i < kContextN; ++i)
-    input >> job.context[i];
-  return input;
-}
-
-std::istream& operator>>(std::istream& input, Batch& batch) {
-  input >> batch.id >> batch.account_id;
-  input >> batch.timely_reward >> batch.reward >> batch.expected_time;
-  input >> batch.due;
-  return input;
-}
-
-std::istream& operator>>(std::istream& input, Machine& machine) {
-  int state;
-  input >> machine.id >> state;
-  machine.state = static_cast<MachineState>(state);
-  return input;
-}
-
-std::istream& operator>>(std::istream& input, MachineSet& set) {
-  input >> set.id;
-
-  std::string line;
-  std::getline(input, line);
-  std::istringstream line_stream(line);
-  int machine_id;
-  while (line_stream >> machine_id) set.machines.push_back(machine_id);
-
-  return input;
-}
-
-std::istream& operator>>(std::istream& input, Account& account) {
-  input >> account.id >> account.alloc;
-  return input;
-}
-
-std::istream& operator>>(std::istream& input, ContextChange& change) {
-  for (int i = 0; i < kContextN; ++i)
-    input >> change.changed[i];
-  input >> change.cost;
-  return input;
-}
-
 }  // namespace io
 }  // namespace lss
