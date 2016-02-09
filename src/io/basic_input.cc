2a2d4ce5b8b02db18fe3f34ee05723ade80b2671
Leonid Logvinov <logvinov.leon@gmail.com>
2016-02-09 15:12:04 +0000

    Fix code style and small issues in basic_input.cc

--- src/io/basic_input.cc
+++ src/io/basic_input.cc
@@ -21,7 +21,7 @@ void ReadOne(std::istream* input, RawData* destination) {
   *input >> (destination->*VEC).back();
 }
 
-using SectionReader = void(*)(std::istream*, RawData*);
+using SectionReader = void (*)(std::istream*, RawData*);
 using HeaderReaderPair = std::tuple<const char*, SectionReader>;
 
 constexpr std::array<HeaderReaderPair, 7> kReaders{
@@ -55,7 +55,7 @@ constexpr std::array<HeaderReaderPair, 7> kReaders{
   },
 };
 
-SectionReader GetSectionReader(const std::string& header) {
+SectionReader GetSectionReader(const std::string &header) {
   for (size_t i = 0; i < kReaders.size(); ++i) {
     if (header == std::get<0>(kReaders[i]))
       return std::get<1>(kReaders[i]);
@@ -65,43 +65,43 @@ SectionReader GetSectionReader(const std::string& header) {
 
 }  // namespace
 
-BasicReader::BasicReader(const std::string& input_path)
-    : input_path_(input_path) { }
+BasicReader::BasicReader(const std::string &input_path)
+  : input_path_(input_path) { }
 
-void BasicReader::SetInputPath(const std::string& input_path) {
+void BasicReader::SetInputPath(const std::string &input_path) {
   input_path_ = input_path;
 }
 
 bool BasicReader::Read(RawData* destination) {
   std::string new_path = input_path_ + ".read";
-  if (std::rename(input_path_.c_str(), new_path.c_str()))
+  if (std::rename(input_path_.c_str(), new_path.c_str())) {
     return false;
+  }
 
-  std::ifstream input(new_path);
-  if (input.fail())
+  std::ifstream input(new_path.c_str());
+  if (input.fail()) {
     return false;
+  }
 
   SectionReader reader = nullptr;
-  std::string line_buf;
-  std::istringstream line;
-  while (getline(input, line_buf)) {
-    if (line_buf.empty())
+  std::string line;
+  while (getline(input, line)) {
+    if (line.empty())
       continue;
 
-    if (SectionReader new_reader = GetSectionReader(line_buf)) {
+    if (SectionReader new_reader = GetSectionReader(line)) {
       reader = new_reader;
     } else if (reader) {
-      line.clear();  // Clear eof flag.
-      line.str(line_buf);
-      reader(&line, destination);
+      std::istringstream line_stream(line);
+      reader(&line_stream, destination);
     } else {
-      LOG(WARNING) << "Expected header in input line: '" << line_buf;
+      LOG(WARNING) << "Expected header in input line: '" << line;
     }
   }
 
   input.close();
   if (std::remove(new_path.c_str())) {
-    LOG(WARNING) << "Failed to remove input file: " << strerror(errno);
+    PLOG(WARNING) << "Failed to remove input file";
   }
 
   return true;
