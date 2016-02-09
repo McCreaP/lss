4540e47d21d80e7a48b0541d7164962a12cc9399
Leonid Logvinov <logvinov.leon@gmail.com>
2016-02-09 15:23:36 +0000

    Reformat main and disable logger buffer

--- src/main.cc
+++ src/main.cc
@@ -2,13 +2,14 @@
 
 #include <glog/logging.h>
 
-void configLogger(char *argv[]) {
+void configLogger(char* argv[]) {
   FLAGS_log_dir = "logs";
   FLAGS_stderrthreshold = 0;
+  FLAGS_logbufsecs = 0;
   google::InitGoogleLogging(argv[0]);
 }
 
-int main(int __attribute__((unused)) argc, char *argv[]) {
+int main(int __attribute__((unused)) argc, char* argv[]) {
   configLogger(argv);
   LOG(INFO) << "Scheduler start";
   const std::string kRunPath = "/home/vagrant/lss/run/";
