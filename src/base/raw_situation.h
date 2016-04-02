25bac4d87f5dbd4b23be71bd25406b4496a50338
Kamil Zyla <7kamilzyla@gmail.com>
2016-04-02 16:33:35 +0200

    Add chainable setters for Raw types.

--- src/base/raw_situation.h
+++ src/base/raw_situation.h
@@ -13,11 +13,19 @@ namespace lss {
 struct RawJob {
   IdType id_;
   IdType batch_;
-  IdType machine_;  // The machine this job is assigned to; not present in the input file.
+  IdType machine_;     // The machine this job is assigned to; not present in the input file.
   Duration duration_;  // Expected duration barring setup time.
   Time start_time_;    // Not present in the input file.
   IdType machine_set_;
   Context context_;
+
+  RawJob& id(IdType val) { id_ = val; return *this; }
+  RawJob& batch(IdType val) { batch_ = val; return *this; }
+  RawJob& machine(IdType val) { machine_ = val; return *this; }
+  RawJob& duration(Duration val) { duration_ = val; return *this; }
+  RawJob& start_time(Time val) { start_time_ = val; return *this; }
+  RawJob& machine_set(IdType val) { machine_set_ = val; return *this; }
+  RawJob& context(Context val) { context_ = val; return *this; }
 
   bool operator==(const RawJob& rhs) const;
 };
@@ -36,6 +44,15 @@ struct RawBatch {
   Duration duration_;
   Time due_;
 
+  RawBatch& id(IdType val) { id_ = val; return *this; }
+  RawBatch& account(IdType val) { account_ = val; return *this; }
+  RawBatch& job_reward(FloatType val) { job_reward_ = val; return *this; }
+  RawBatch& job_timely_reward(FloatType val) { job_timely_reward_ = val; return *this; }
+  RawBatch& reward(FloatType val) { reward_ = val; return *this; }
+  RawBatch& timely_reward(FloatType val) { timely_reward_ = val; return *this; }
+  RawBatch& duration(Duration val) { duration_ = val; return *this; }
+  RawBatch& due(Time val) { due_ = val; return *this; }
+
   bool operator==(const RawBatch& rhs) const;
 };
 
@@ -43,21 +60,34 @@ struct RawMachine {
   IdType id_;
   MachineState state_;
   Context context_;  // Not present in the input file.
+
+  RawMachine& id(IdType val) { id_ = val; return *this; }
+  RawMachine& state(MachineState val) { state_ = val; return *this; }
+  RawMachine& context(Context val) { context_ = val; return *this; }
 };
 
 struct RawMachineSet {
   IdType id_;
   std::vector<IdType> machines_;
+
+  RawMachineSet& id(IdType val) { id_ = val; return *this; }
+  RawMachineSet& add(IdType val) { machines_.push_back(val); return *this; };
 };
 
 struct RawAccount {
   IdType id_;
   FloatType alloc_;
+
+  RawAccount& id(IdType val) { id_ = val; return *this; }
+  RawAccount& alloc(FloatType val) { alloc_ = val; return *this; }
 };
 
 struct RawChangeCost {
   Change change_;
   Cost cost_;
+
+  RawChangeCost& change(Change val) { change_ = val; return *this; }
+  RawChangeCost& cost(Cost val) { cost_ = val; return *this; }
 };
 
 struct RawSituation {
